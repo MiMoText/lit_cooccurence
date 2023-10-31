2469cf1831f0753b7e9da240d918f74142464eb5
duan <duan@uni-trier.de>
2023-10-31 16:19:35 +0100

    add npmi plots

--- pmi.py
+++ pmi.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
 import numpy as np
+import plotly.graph_objects as go
+
 
 df = pd.read_csv("work_title.tsv", sep="\t")
 df = df[df.type == "Literary work"]
@@ -15,19 +17,20 @@ for work in all_works:
 
 unique_text = df.text.unique() #get a list of all secondary works
 
+umfang = "text" # set it to "sentence" in order to get only the cooccurrence in the same sentence 
 
 for title in unique_text:
-    ner_list = df[df.text == title].Identifier.unique() #get a list of unique literary works mentioned in the secondary work
-    for work in all_works:
-        if work in ner_list:
-            work_index[work].add(title)
-
-# for title in unique_text:
-#     for sentence in df[df.text == title].sentence.unique():
-#         ner_list = df[df.text == title][df.sentence == sentence].Identifier.unique() #get a list of unique literary works mentioned in the secondary work
-#         for work in all_works:
-#             if work in ner_list:
-#                 work_index[work].add(title+"_"+str(sentence))
+    if umfang == "text":
+        ner_list = df[df.text == title].Identifier.unique() #get a list of unique literary works mentioned in the secondary work
+        for work in all_works:
+            if work in ner_list:
+                work_index[work].add(title)
+    else:
+        for sentence in df[df.text == title].sentence.unique():
+            ner_list = df[df.text == title][df.sentence == sentence].Identifier.unique()
+            for work in all_works:
+                if work in ner_list:
+                    work_index[work].add(title+"_"+str(sentence))
 
 n = len(all_works)
 npmi_matrix = np.zeros((n, n))
@@ -47,12 +50,41 @@ for i in range(n):
         npmi_matrix[i][j] = npmi
         npmi_matrix[j][i] = npmi
 
-#plot matrix as heatmap
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
-ax = sns.heatmap(npmi_matrix, xticklabels=all_works, yticklabels=all_works)
-plt.show()
+# Assuming npmi_matrix is already computed and is a numpy array
+x_labels = all_works
+y_labels = all_works
+
+custom_colorscale = [
+    [0.0, 'rgb(255,255,255)']  # white for zero
+    # You can add more colors for other values here.
+    # For example:
+    # [0.1, 'rgb(235, 237, 240)'],
+    # [1.0, 'rgb(0, 0, 255)']
+    # Ensure that the colorscale covers the range from the minimum to the maximum
+    # values that your npmi_matrix can take, excluding zero if it has a special color.
+]
+
+fig = go.Figure(data=go.Heatmap(
+                    z=npmi_matrix,
+                    x=x_labels,
+                    y=y_labels,
+                    colorscale=custom_colorscale,
+                    zmin=-1,  # Assuming the NPMI values range from -1 to 1
+                    zmax=1,
+                    colorbar=dict(tickvals=[0], ticktext=['No Cooccurrence'])))
+
+# Update layout to make it more readable
+fig.update_layout(
+    title=f'NPMI Heatmap of Works (cooccurrence in {umfang})',
+    xaxis_nticks=36,
+    yaxis_nticks=36,
+    xaxis_title="Work 1",
+    yaxis_title="Work 2",
+    margin=dict(l=150, r=5, t=5, b=150)
+)
+
+# Export to HTML
+fig.write_html(f'npmi_heatmap_{umfang}.html')
 
 
 
